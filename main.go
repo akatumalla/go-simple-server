e194bdb08df3832821cdd310af80356ec53c8858
akatumalla <akatumalla@gmail.com>
2021-05-05 20:53:59 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+
+	"log"
+
+	"net/http"
+	"os"
+
+
+	"github.com/joho/godotenv"
+
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Simple Go server deployed using CircleCI!</h1>"))
+}
+
+func main() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+
+		log.Println("Error loading .env file")
+
+	}
+
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "11000"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
+
